47da31b3902da68b5230643bf8979afceb99f998
allan <AllanTo@magenic.com>
2022-02-13 05:55:27 +0800

    Added sample unit test

--- A.Blockchain.API.Tests/BlockchainControllerTests.cs
+++ A.Blockchain.API.Tests/BlockchainControllerTests.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace A.Blockchain.API.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class BlockchainControllerTests
     {
         [TestMethod]
         public void TestMethod1()
---
+++ A.Blockchain.Service.Tests/BlockchainServiceTests.cs
@@ -0,0 +1,19 @@
+using A.Blockchain.Core.Interfaces.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace A.Blockchain.Service.Tests
+{
+    [TestClass]
+    public class BlockchainServiceTests
+    {
+        [TestMethod]
+        public void CreateGenesisBlock()
+        {
+            var blockchainRepositoryMock = new Mock<IBlockchainRepository>();
+            var blockchainService = new BlockchainService(blockchainRepositoryMock.Object);
+
+            blockchainService.CreateGenesisBlock();
+        }
+    }
+}
